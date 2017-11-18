9b8a5f0450efeb041cc659c47eac0b368499669b
zhiminsim <zhiminsim@gmail.com>
2017-11-18 16:28:22 +0800

    1. Display original image on screen 2. Threshold the image and convert into binary image 3. Determine outline 4. Segment the image to seperate and identify the different characters. 5. Rotation of the characters about their own respective centroids by 90 degrees CW 6. Rotation of the characters about their own respective centroids by 30 degrees CCW 7. Determine a one-pixel thin image of the characters from Step 4.

--- readimage_zm.m
+++ readimage_zm.m
@@ -11,25 +11,30 @@ function readimage_zm
     
     image2 = smoothImageEdge (image2);
     image2 = removeSmall (image2);
+    
+    %image pre-processing and filtering done above this line
+    
+    boundaryTrace(image1);
+    boundaryTrace(image2);
 
     array1 = connectedComponents(image1);
-    
+   %{ 
     for i = 1:numel(array1)
         figure
         imshow(array1{i})
     end
-    
+    %}
     array2 = connectedComponents (image2);
-    
+    %{ 
     for i = 1:numel(array2)
         figure
         imshow(array2{i})
     end
-    
-    rotateSegment90cw(array1);
-    rotateSegment90cw(array2);
-    rotateSegment30ccw(array1);
-    rotateSegment30ccw(array2);
+    %}
+    %rotateSegment90cw(array1);
+    %rotateSegment90cw(array2);
+    %rotateSegment30ccw(array1);
+    %rotateSegment30ccw(array2);
 
 end
 
@@ -50,14 +55,17 @@ function sI = smoothImageEdge (img) %smooth out the edges of the characters with
 end
 
 function reArray = connectedComponents (img) %create connectivity factor and segment into RGB colours
+    initialDisp =  zeros(size(img));
+
     
-    CC = bwconncomp (img, 8);
+    CC = bwconncomp (img, 26);
     reArray = cell(1,(CC.NumObjects) * 2); 
-    var = (regionprops (CC, 'BoundingBox'));
-    var(1).BoundingBox;
+    rotatedImagesArray = cell(1,(CC.NumObjects) * 2); 
+    rotatedImagesArray2 = cell(1,(CC.NumObjects) * 2); 
+    var = (regionprops (CC, 'BoundingBox', 'Image'));
     q=1;
     
-    for k = 1:CC.NumObjects %// Loop through each object and plot it in white. 
+    for k = 1:CC.NumObjects %{ Loop through each object and plot it in white. 
                                 %This is where you can create individual figures for each object.
 
         PixId = CC.PixelIdxList{k}; %// Just simpler to understand
@@ -70,29 +78,113 @@ function reArray = connectedComponents (img) %create connectivity factor and seg
             BW2(PixId) = 255;
             BW2 = imcrop(BW2, cropArea);
             NArea = bwarea (BW2);
-            
             if NArea > 5000
                n = fix(size(BW2,1)/2);
-               A = BW2(:,1:end/2);
-               B = BW2(:,end/2+1:end);
+               A = BW2(:,1:(end/2) - 4);
+               B = BW2(:,end/2+1-4:end);
                 
                reArray{q}= A;
                reArray{q+1} = B;
                q=q+2;
+               
+                % ROTATION HERE (-90)
+                C = imrotate(A, -90);
+                C = insertIntoImage(initialDisp, C, cropArea(2), cropArea(1));
+                D = imrotate(B, -90);
+                D = insertIntoImage(initialDisp, D, cropArea(2), cropArea(1) + (cropArea(4) / 2));
+
+               rotatedImagesArray{q-2}= C;
+               rotatedImagesArray{q-1} = D;
+               
+                % ROTATION HERE (30)
+                E = imrotate(A, 30);
+                E = insertIntoImage(initialDisp, E, cropArea(2), cropArea(1));
+                F = imrotate(B, 30);
+                F = insertIntoImage(initialDisp, F, cropArea(2), cropArea(1) + (cropArea(4) / 2));
+
+               rotatedImagesArray2{q-2}= E;
+               rotatedImagesArray2{q-1} = F;
+               
                 
             else            
                 reArray{q} = BW2;
                 q=q+1;
+                
+                % ROTATION HERE (-90)
+                A = imrotate(BW2, -90);
+                B = insertIntoImage(initialDisp, A, cropArea(2), cropArea(1));
+                rotatedImagesArray{q-1}= B;
+                
+                % ROTATION HERE (30)
+                C = imrotate(BW2, 30);
+                D = insertIntoImage(initialDisp, C, cropArea(2), cropArea(1));
+                rotatedImagesArray2{q-1}= D;
+                
             end
         end
     end
     
+    % RESIZING CELL ARRAYS
     reArray = reArray(1:(q-1));
+    rotatedImagesArray = rotatedImagesArray(1:(q-1));
+    rotatedImagesArray2 = rotatedImagesArray2(1:(q-1));
+    
+    % DISPLAY -90 DEG ROTATION
+    initialDisp = findLargestBackground(rotatedImagesArray);
+    for k = 1:q-1
+       newimg = rotatedImagesArray{k};
+       newimgsize = size(newimg);
+       initialDisp(1:newimgsize(1), 1:newimgsize(2)) = initialDisp(1:newimgsize(1), 1:newimgsize(2)) + newimg ;
+    end
+    figure
+    imshow(initialDisp)
+    
+    % DISPLAY 30 DEG ROTATION
+    initialDisp = findLargestBackground(rotatedImagesArray2);
+    for k = 1:q-1
+       newimg = rotatedImagesArray2{k};
+       newimgsize = size(newimg);
+       initialDisp(1:newimgsize(1), 1:newimgsize(2)) = initialDisp(1:newimgsize(1), 1:newimgsize(2)) + newimg ;
+    end
+    figure
+    imshow(initialDisp)
+    
+    % Gets OCRed version of image. Need to get the ".Word" component out
+    % from 
+    for k = 1:q-1
+        figure
+        A = padarray(reArray{k}, [5 5]) ;
+        imshow(A)
+        ocrres = ocr(A, 'TextLayout', 'Block', 'CharacterSet', '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ');
+        word = ocrres.Words{1};
+    end
+    
 
     %labeled = labelmatrix (CC);
     %RGB_label = label2rgb(labeled);    
     %imshow (RGB_label);
     %RGB_label = bwareaopen(img, 200);
+end
+
+function newImg = insertIntoImage(baseImg, imageToInsert, startrow, startcol) 
+    newImg =  zeros(size(baseImg));
+    newImg(startrow:startrow+size(imageToInsert,1)-1,startcol:startcol+size(imageToInsert,2)-1) = imageToInsert;
+end
+
+% Create an image with a large enough background to contain all images in
+% the input cell array
+function largeEnoughBackground = findLargestBackground(rotatedImagesArray)
+    maxDim = 0;
+    for k = 1:length(rotatedImagesArray)
+        maxDim = max(maxDim, size(rotatedImagesArray{k}));
+    end
+    largeEnoughBackground = zeros(maxDim);
+end
+
+function boundaryTrace(image) %trace the perimeter of the binary image argument
+    bT = bwperim(image, 8);
+    figure
+    imshow(bT)
 end
 
 function rotateSegment90cw(array)
